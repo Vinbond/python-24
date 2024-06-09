15027fe3d8b4b0d09021d6499d7a673984c2f628
vinbond <vinayakkundangar77@gmail.com>
2024-06-09 23:52:58 +0530

    pi

--- IOoperations.py
+++ IOoperations.py
@@ -1,3 +1,4 @@
+import pickle
 # f=open("Normal.text","w")
 # # g=f.read()
 #
@@ -8,7 +9,23 @@
 # m=open("Normal.text","r")
 # n=m.read()
 # print(n)
-with open("N2.py","a") as y:
-    y.write("This is new line\n")
+# with open("N2.py","a") as y:
+#     y.write("This is new line\n")
+#pickle  Module
+
+# li=["One","Two","Three",4,5,6]
+# f_obj=pickle.dumps(li)
+# print("File Objets=>", f_obj)
+# unpic=pickle.loads(f_obj)
+# print("unpickling+>",unpic)
+# hh="first.pickle"
+# obj=[1,2,3,4,5,6,7]
+# pickle.dump(obj,open("first.pickle","wb"))
+# jj=open(hh,"rb")
+# ss=pickle.load(jj)
+# print(ss)
+
+
+
 
 
